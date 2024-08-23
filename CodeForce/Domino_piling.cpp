15b786dd1f62632172472079f756d11a6bfee9f7
0xaman47 <0xaman47@gmail.com>
2024-08-23 11:20:33 +0530

    Codeforce : Ratting 800 - Domino Piling

--- CodeForce/Domino_piling.cpp
+++ CodeForce/Domino_piling.cpp
@@ -51,14 +51,40 @@ m = 3    |__I|
     Here '__' denote vertical domino polling and 'I' denote vertical domino polling
 */
 
+/*
+Question : 
+*/
+
+/*
+Solution
+*/
+
 #include<bits/stdc++.h>
+#include<algorithm>
+#include<string>
+#include<vector>
 using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define arrInp for(int i=0;i<n;i++) cin >> arr[i];
+#define arrOut(k) for(int i=0;i<n;i++) cout << arr[i] <<  k;
+#define el cout << endl;
 
-int main(){
-
+void fun(){
     float m,n;
     cin>>m>>n;
     n = (n/2)*m;
     cout<<floor(n);
+}
+
+int main(){
+
+    int t = 1; 
+    //cin >> t;
+    while(t--){
+     fun();
+    }
+
   return 0;
 }
