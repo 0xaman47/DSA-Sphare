f32a1cd73ae9e551f3de524859459fde18d2c842
0xaman47 <0xaman47@gmail.com>
2024-08-30 11:21:03 +0530

    commit

---
+++ CodeForce/George_and_Accommodation.cpp
@@ -0,0 +1,44 @@
+/*
+Question : 
+*/
+
+/*
+Solution
+*/
+
+#include<bits/stdc++.h>
+#include<algorithm>
+#include<string>
+#include<vector>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define arrInp for(int i=0;i<n;i++) cin >> arr[i];
+#define arrOut(k) for(int i=0;i<n;i++) cout << arr[i] <<  k;
+#define el cout << endl;
+
+void fun(){
+    int n;
+    cin>>n;
+    int a,b,count = 0;;
+    for(int i = 0; i < n; i++){
+        cin>>a>>b;
+        a = a + 1;
+        if(a < b){
+            count++;
+        }
+    }
+    cout<<count<<endl;
+}
+
+int main(){
+
+    int t = 1; 
+    //cin >> t;
+    while(t--){
+     fun();
+    }
+
+  return 0;
+}
