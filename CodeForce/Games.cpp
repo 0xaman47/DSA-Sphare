4b915a2df018e352e7657dc5cfef67e46ce278f6
0xaman47 <0xaman47@gmail.com>
2024-09-28 23:40:39 +0530

    Codeforce : Ratting 800 - Games

---
+++ CodeForce/Games.cpp
@@ -0,0 +1,54 @@
+/*
+Question : 
+*/
+
+/*
+Solution
+*/
+
+#include<bits/stdc++.h>
+#include<algorithm>
+#include<string>
+#include<vector>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define arrInp for(int i=0;i<n;i++) cin >> arr[i];
+#define arrOut(k) for(int i=0;i<n;i++) cout << arr[i] <<  k;
+#define el cout << endl;
+
+void fun(){
+     int n;
+    cin >> n;
+
+    int number(0);
+    vector<int> h(n), a(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> h[i] >> a[i];
+        for (int j = 0; j < i; ++j)
+        {
+            if (h[i] == a[j])
+            {
+                number += 1;
+            }
+            if (a[i] == h[j])
+            {
+                number += 1;
+            }
+        }
+    }
+    cout << number << endl;
+}
+
+int main(){
+
+    int t = 1; 
+    //cin >> t;
+    while(t--){
+     fun();
+    }
+
+  return 0;
+}
